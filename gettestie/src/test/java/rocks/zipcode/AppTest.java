2ebd8cc7f5cff4db55b78074fa6a8b4a54a73b00
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-29 16:44:53 -0500

    stubbed tests

--- gettestie/src/test/java/rocks/zipcode/AppTest.java
+++ gettestie/src/test/java/rocks/zipcode/AppTest.java
@@ -8,11 +8,31 @@ import static org.junit.Assert.*;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test.
-     */
+
     @Test
-    public void testApp() {
-        assertTrue(true);
-    }
+    public void HashSetTest(){}
+
+    public void ArrayListTest(){}
+
+    public void HashMapTest(){}
+
+    public void LinkedListTest(){}
+
+    public void ArrayDequeTest(){}
+
+    public void VectorTest(){}
+
+    public void TreeMapTest(){}
+
+    public void StackTest(){}
+
+    public void TreeSetTest(){}
+
+    public void PriorityQueueTest(){}
+
+    public void Comparable(){}
+
+
+
+
 }
